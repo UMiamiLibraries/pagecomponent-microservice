5da98a5d5ad7f44a1bff18a09c33abaa353cd322
cbrownroberts <charlesbrownroberts@miami.edu>
2016-10-13 14:57:54 -0400

    render class for writing html views to dist

--- src/Uml/Pagecomponent/Writer.php
+++ src/Uml/Pagecomponent/Writer.php
@@ -8,5 +8,54 @@
  */
 class Writer
 {
+    public function __construct() {
+    }
 
+
+    public function getJson() {
+        $url = "assets/data/components.json";
+        $json = file_get_contents($url);
+        $jsonArray = json_decode($json, true);
+        return $jsonArray;
+    }
+
+    public function write($source, $destination) {
+
+        if(file_exists($source)) {
+
+            $data = file_get_contents($source);
+
+            if (!file_exists($destination)) {
+                touch($destination);
+            }
+
+            file_put_contents($destination, $data);
+        }
+
+
+    }
+
+    public function writeComponents() {
+
+        $array = $this->getJson();
+
+
+        foreach($array as $component):
+
+            //var_dump($component);
+            $src = $component['src'];
+            $dist = $component['dist'];
+            $filename = $component['filename'];
+
+            $source      = $src . '/' . $filename;
+            $destination = $dist . '/' . $filename;
+
+            $this->write($source, $destination);
+
+        endforeach;
+
+    }
 }
+
+$w = new Writer();
+$w->writeComponents();
